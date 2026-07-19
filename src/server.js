46e8eaa74647e491d02d7b927daba81353f1bf43
1-dara <1ireneokon@gmail.com>
2026-07-19 07:48:58 +0200

    Fix duplicate app declaration in server.js

--- src/server.js
+++ src/server.js
@@ -1,73 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import authRoutes from './routes/authRoutes.js';
-import taskRoutes from './routes/taskRoutes.js';
-import cors from 'cors';
 import app from './app.js';
-import rateLimit from 'express-rate-limit';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Docs available at http://localhost:${PORT}/docs`);
 });
-
-const app = express();
-app.use(express.json());
-const limiter = rateLimit({
-    windowMs: 60 * 1000,
-    max: 100,
-    message: { error: 'Too many requests, please try again later' }
-});
-app.use(limiter);
-
-app.use(cors());
-
-
-// Swagger setup
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Task Manager API',
-            version: '1.0.0',
-            description: 'A task management REST API built with Node.js, Express and Prisma',
-        },
-        servers: [
-            {
-                url: 'https://task-manager-api-n7wv.onrender.com',
-                description: 'Production server',
-            },
-            {
-                url: 'http://localhost:3000',
-                description: 'Local server',
-            },
-        ],
-
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                },
-            },
-        },
-    },
-    apis: ['./src/routes/*.js'],
-};
-
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.get('/', (req, res) => {
-    res.redirect('/docs');
-});
-
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
-
